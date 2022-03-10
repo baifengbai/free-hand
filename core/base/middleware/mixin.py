7e6842754b5ad18438a76c90b9f62086350c0c0f
Eighteen <dreamertgq@163.com>
2022-03-10 16:59:40 +0800

    20220310 新增相似度判断字符串是否是链接方法 middleware mixin

--- core/base/middleware/mixin.py
+++ core/base/middleware/mixin.py
@@ -1,3 +1,5 @@
+from utils.common import IsCheck_uchar
+
 """基类"""
 class MiddlewareMixin:
     """
@@ -15,3 +17,20 @@ class MiddlewareMixin:
         """集成操作，子类需要重写该方法"""
         obj = self.process_default(obj)
         return obj
+
+    def _str_check(self, s, is_which='url'):
+        """相似度判断字符串是否是链接，字母占比超过80%判定为链接
+        :param s 待判定的字符串 【注意】 该字符串需要经过清洗之后再传进来
+        :param is_which 是否为某某 如 url
+        :output bool值
+        """
+        if(is_which=='url'):
+            alpha_num = 0
+            for c in s:
+                if(IsCheck_uchar.is_alphabet(c)):
+                    alpha_num = alpha_num + 1
+            ratio = alpha_num/len(s)
+            if(ratio>0.75):
+                return True
+            else:
+                return False
